b548a399168161de4348704749a5c97f78ef23a7
jixiewang201907 <63153938+jixiewang201907@users.noreply.github.com>
2021-12-22 21:00:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-
 #sirpdboy
 #git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
 #git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
