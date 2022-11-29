a5cac20041bb6a23085ccced961c4842f7882dbb
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2022-11-29 19:35:47 +0800

    Update diy-imm-part1.sh

--- diy-imm-part1.sh
+++ diy-imm-part1.sh
@@ -14,5 +14,5 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld -b 20221031 https://github.com/jixiewang201907' >>feeds.conf.default
+echo 'src-git helloworld -b --20221031 https://github.com/jixiewang201907' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
