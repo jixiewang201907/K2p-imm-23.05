536c5e27db54c127c6fe287cc93f9a5cd62bc099
Padavan_Auto_Build_4.4 <63153938+jixiewang201907@users.noreply.github.com>
2022-12-03 11:42:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
